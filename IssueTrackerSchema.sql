e12722c2eca9d19dd2ba657b51a149e643c87487
fengt256512 <93305654+fengt256512@users.noreply.github.com>
2022-03-17 12:02:48 +1100

    Add files via upload

---
+++ IssueTrackerSchema.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS A3_ISSUE;
+DROP TABLE IF EXISTS A3_USER;
+
+
+
+CREATE TABLE A3_USER
+(
+	USER_ID SERIAL primary key,
+	USERNAME VARCHAR(100) not null,
+	FIRSTNAME VARCHAR(100) not null, 
+	LASTNAME VARCHAR(100) not null
+);
+
+Insert into A3_USER (USERNAME,FIRSTNAME,LASTNAME) values ('-','not','assigned');			-- 1
+Insert into A3_USER (USERNAME,FIRSTNAME,LASTNAME) values ('Chris', 'Christopher','Smith');  -- 2
+Insert into A3_USER (USERNAME,FIRSTNAME,LASTNAME) values ('Dave', 'David','Jones');         -- 3
+Insert into A3_USER (USERNAME,FIRSTNAME,LASTNAME) values ('Vlad', 'Vladimir','Putin');      -- 4
+
+CREATE TABLE A3_ISSUE 
+(
+	ISSUE_ID SERIAL primary key,
+	TITLE VARCHAR(100),  
+	DESCRIPTION VARCHAR(1000),
+	CREATOR INTEGER not null REFERENCES A3_USER, 
+	RESOLVER INTEGER REFERENCES A3_USER, 
+	VERIFIER INTEGER REFERENCES A3_USER
+);
+
+Insert into A3_ISSUE (TITLE,DESCRIPTION,CREATOR,RESOLVER,VERIFIER) values ('Factorial with addition anomaly','Performing a factorial and then addition produces an off by 1 error',2,3,4);
+Insert into A3_ISSUE (TITLE,DESCRIPTION,CREATOR,RESOLVER,VERIFIER) values ('Division by zero','Division by 0 doesn''t yield error or infinity as would be expected. Instead it results in -1.',2,3,1);
+Insert into A3_ISSUE (TITLE,DESCRIPTION,CREATOR,RESOLVER,VERIFIER) values ('Incorrect BODMAS order','Addition occurring before multiplication',3,1,1);
+
+commit;
+
+
+
+
+
+CREATE OR REPLACE FUNCTION Search1(IN serachString VARCHAR) returns refcursor
+AS $$
+DECLARE
+	refc1 refcursor;
+BEGIN
+	OPEN  refc1 FOR
+	SELECT ISSUE_ID,TITLE,DESCRIPTION,CREATOR,RESOLVER,VERIFIER
+	FROM A3_ISSUE
+	WHERE  TITLE LIKE '%'||serachString||'%'
+	ORDER BY TITLE;
+	return refc1;
+END; $$ LANGUAGE plpgsql;
